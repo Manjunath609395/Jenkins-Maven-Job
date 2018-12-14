96d6bad2675c0a9522a5ba420894de6123772843
Manjunath Avesani <manjunath.avesani@wkelms.com>
2018-12-14 11:19:23 +0530

    Maven First Commit

---
+++ src/main/java/com/firstdocker/springboot/dockerservice/DockerServiceApplication.java
@@ -0,0 +1,29 @@
+package com.firstdocker.springboot.dockerservice;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DockerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerServiceApplication.class, args);
+	}
+
+	@RestController
+	class HelloDockerRestController {
+		@RequestMapping("/hello/{name}")
+		public String helloDocker(@PathVariable(value = "name") String name) {
+			String response = "Hello " + name + " Response received on : " + new Date();
+			System.out.println(response);
+			return response;
+
+		}
+	}
+
+}
